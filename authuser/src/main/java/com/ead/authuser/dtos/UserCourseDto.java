ae3288787c9f3faf0409ea30fa36314c53f5efda
Silvio Bassi <silviobassi1@hotmail.com>
2023-10-29 20:32:59 -0300

    AuthUser asynchronous communication

--- authuser/src/main/java/com/ead/authuser/dtos/UserCourseDto.java
+++
@@ -1,9 +0,0 @@
-package com.ead.authuser.dtos;
-
-import jakarta.validation.constraints.NotNull;
-
-import java.io.Serializable;
-import java.util.UUID;
-
-public record UserCourseDto(@NotNull UUID courseId, UUID userId) implements Serializable {
-}
