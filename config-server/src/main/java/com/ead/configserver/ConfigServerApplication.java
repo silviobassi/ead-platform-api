8d7fa2cb8046b911e014f2a2b22d798487c1c97f
Silvio Bassi <silviobassi1@hotmail.com>
2023-11-09 20:02:30 -0300

    update dev properties

--- config-server/src/main/java/com/ead/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/ead/configserver/ConfigServerApplication.java
@@ -1,6 +1,5 @@
 package com.ead.configserver;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
